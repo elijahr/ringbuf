bbf582dacec568d8d8d076a730f0c80365b56c78
Elijah Shaw-Rutschman <elijahr@gmail.com>
2022-09-30 01:38:47 -0500

    Check compiler

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
+import glob
+import itertools
 import os
-import platform
 
 from Cython.Build import (
     cythonize,
@@ -7,6 +8,9 @@ from Cython.Build import (
 from setuptools import (
     Extension,
     setup,
+)
+from setuptools.command.build_ext import (
+    build_ext,
 )
 
 compile_time_env = {
@@ -21,38 +25,60 @@ try:
 except ImportError:
     ...
 
-include_dirs = []
-extra_compile_args = []
-extra_link_args = []
 
+class build_ext_compiler_check(build_ext):
+    def build_extensions(self):
+        compiler_type = self.compiler.compiler_type  # usually "unix" or "msvc"
 
-if boost_root := os.environ.get("BOOST_ROOT"):
-    # Try to find the include dir
-    headers = (
-        os.path.join(boost_root, "boost", "lockfree", "spsc_queue.hpp"),
-        os.path.join(boost_root, "include", "boost", "lockfree", "spsc_queue.hpp"),
-    )
-    for header in headers:
-        if os.path.exists(header):
-            include_dir = os.path.join(*header.split(os.path.sep)[:-3])
-            include_dirs.append(include_dir)
-            extra_compile_args.append(f"-I{include_dir}")
-            break
-    else:
-        raise ValueError(f"Could not find include directory in {boost_root}")
+        if boost_root := os.environ.get("BOOST_ROOT"):
+            ringbufcy_ext = [e for e in self.extensions if e.name == "ringbuf.ringbufcy"][0]
 
-    link_flag = "/LIBPATH:" if platform.system() == "Windows" else "-L"
-    lib_dirs = (
-        os.path.join(boost_root, "lib"),
-        os.path.join(boost_root, "lib64"),
-        os.path.join(boost_root, "lib32"),
-    )
-    for lib_dir in lib_dirs:
-        if os.path.exists(lib_dir):
-            extra_link_args.append(f"{link_flag}{lib_dir}")
-            break
-    else:
-        raise ValueError(f"Could not find lib directory in {boost_root}")
+            # Try to find the include dir
+            for header in (
+                os.path.join(
+                    boost_root,
+                    "boost",
+                    "lockfree",
+                    "spsc_queue.hpp",
+                ),
+                os.path.join(
+                    boost_root,
+                    "include",
+                    "boost",
+                    "lockfree",
+                    "spsc_queue.hpp",
+                ),
+            ):
+                if os.path.exists(header):
+                    include_dir = os.path.join(*header.split(os.path.sep)[:-3])
+                    ringbufcy_ext.include_dirs.append(include_dir)
+                    ringbufcy_ext.extra_compile_args.append(f"-I{include_dir}")
+                    break
+            else:
+                raise ValueError(f"Could not find headers with BOOST_ROOT={boost_root}")
+
+            # Try to find the library dir
+            for lib_dir in itertools.chain.from_iterable(
+                map(
+                    glob.glob,
+                    (
+                        os.path.join(boost_root, "lib"),
+                        os.path.join(boost_root, "lib64"),
+                        os.path.join(boost_root, "lib32"),
+                        os.path.join(boost_root, "lib64-*"),
+                        os.path.join(boost_root, "lib32-*"),
+                    ),
+                )
+            ):
+                if os.path.exists(lib_dir) and os.path.isdir(lib_dir):
+                    link_flag = "/LIBPATH:" if compiler_type == "msvc" else "-L"
+                    ringbufcy_ext.extra_link_args.append(f"{link_flag}{lib_dir}")
+                    break
+            else:
+                raise ValueError(f"Could not find libraries with BOOST_ROOT={boost_root}")
+
+        build_ext.build_extensions(self)
+
 
 setup(
     ext_modules=cythonize(
@@ -66,14 +92,12 @@ setup(
                 "ringbuf.ringbufcy",
                 sources=["ringbuf/ringbufcy.pyx"],
                 language="c++",
-                extra_compile_args=extra_compile_args,
-                extra_link_args=extra_link_args,
                 libraries=["boost_system"],
-                include_dirs=include_dirs,
             ),
         ],
         gdb_debug=compile_time_env["DEBUG"],
         compile_time_env=compile_time_env,
         compiler_directives={"language_level": 3},
     ),
+    cmdclass={"build_ext": build_ext_compiler_check},
 )
