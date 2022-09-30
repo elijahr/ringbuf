b87c31adcf250ac2ec33c1fa5f1d1ccbe29e0bf9
Elijah Shaw-Rutschman <elijahr@gmail.com>
2022-09-30 02:42:34 -0500

    Better lib and include searching

--- setup.py
+++ setup.py
@@ -26,6 +26,85 @@ except ImportError:
     ...
 
 
+def find_spsc_queue_hpp(boost_root):
+    for header in (
+        os.path.join(
+            boost_root,
+            "boost",
+            "lockfree",
+            "spsc_queue.hpp",
+        ),
+        os.path.join(
+            boost_root,
+            "include",
+            "boost",
+            "lockfree",
+            "spsc_queue.hpp",
+        ),
+    ):
+        if os.path.exists(header):
+            yield header
+
+
+def find_boost_system_lib_msvc(boost_root):
+    for lib_dir in itertools.chain.from_iterable(
+        map(
+            glob.glob,
+            (
+                os.path.join(boost_root, "lib"),
+                os.path.join(boost_root, "lib64"),
+                os.path.join(boost_root, "lib64-*"),
+                os.path.join(boost_root, "lib32"),
+                os.path.join(boost_root, "lib32-*"),
+            ),
+        )
+    ):
+        for lib in itertools.chain.from_iterable(
+            map(
+                glob.glob,
+                (
+                    # Prefer multi-threaded
+                    os.path.join(lib_dir, "boost_system-*mt*.dll"),
+                    os.path.join(lib_dir, "libboost_system-*mt*.dll"),
+                    os.path.join(lib_dir, "boost_system-*.dll"),
+                    os.path.join(lib_dir, "libboost_system-*.dll"),
+                    os.path.join(lib_dir, "boost_system.dll"),
+                    os.path.join(lib_dir, "libboost_system.dll"),
+                ),
+            )
+        ):
+            yield lib
+
+
+def find_boost_system_lib_unix(boost_root):
+    for lib_dir in itertools.chain.from_iterable(
+        map(
+            glob.glob,
+            (
+                os.path.join(boost_root, "lib"),
+                os.path.join(boost_root, "lib64"),
+                os.path.join(boost_root, "lib32"),
+            ),
+        )
+    ):
+        for ext in (
+            "so",  # shared on Linux, BSD
+            "dylib",  # shared on macOS
+        ):
+            for lib in itertools.chain.from_iterable(
+                map(
+                    glob.glob,
+                    (
+                        # Prefer multi-threaded
+                        os.path.join(lib_dir, f"libboost_system-*mt*.{ext}"),
+                        os.path.join(lib_dir, f"libboost_system-*.{ext}"),
+                        os.path.join(lib_dir, f"libboost_system.{ext}"),
+                    ),
+                )
+            ):
+                yield lib
+
+
 class build_ext_compiler_check(build_ext):
     def build_extensions(self):
         compiler_type = self.compiler.compiler_type  # usually "unix" or "msvc"
@@ -34,48 +113,35 @@ class build_ext_compiler_check(build_ext):
             ringbufcy_ext = [e for e in self.extensions if e.name == "ringbuf.ringbufcy"][0]
 
             # Try to find the include dir
-            for header in (
-                os.path.join(
-                    boost_root,
-                    "boost",
-                    "lockfree",
-                    "spsc_queue.hpp",
-                ),
-                os.path.join(
-                    boost_root,
-                    "include",
-                    "boost",
-                    "lockfree",
-                    "spsc_queue.hpp",
-                ),
-            ):
-                if os.path.exists(header):
-                    include_dir = os.path.join(*header.split(os.path.sep)[:-3])
-                    ringbufcy_ext.include_dirs.append(include_dir)
-                    ringbufcy_ext.extra_compile_args.append(f"-I{include_dir}")
-                    break
+            for header in find_spsc_queue_hpp(boost_root):
+                include_dir = os.path.join(*header.split(os.path.sep)[:-3])
+                ringbufcy_ext.include_dirs.append(include_dir)
+                ringbufcy_ext.extra_compile_args.append(f"-I{include_dir}")
+                break
             else:
-                raise ValueError(f"Could not find headers with BOOST_ROOT={boost_root}")
+                raise ValueError(f"Could not find boost/lockfree/spsc_queue.hpp in BOOST_ROOT ({boost_root})")
 
             # Try to find the library dir
-            for lib_dir in itertools.chain.from_iterable(
-                map(
-                    glob.glob,
-                    (
-                        os.path.join(boost_root, "lib"),
-                        os.path.join(boost_root, "lib64"),
-                        os.path.join(boost_root, "lib32"),
-                        os.path.join(boost_root, "lib64-*"),
-                        os.path.join(boost_root, "lib32-*"),
-                    ),
-                )
-            ):
-                if os.path.exists(lib_dir) and os.path.isdir(lib_dir):
-                    link_flag = "/LIBPATH:" if compiler_type == "msvc" else "-L"
-                    ringbufcy_ext.extra_link_args.append(f"{link_flag}{lib_dir}")
+            if compiler_type == "msvc":
+                for lib in find_boost_system_lib_msvc(boost_root):
+                    lib_dir = os.path.dirname(lib)
+                    ringbufcy_ext.extra_link_args.append(f"/LIBPATH:{lib_dir}")
+                    lib_name = os.path.splitext(os.path.basename(lib))[0]
+                    ringbufcy_ext.libraries = [lib_name]
                     break
+                else:
+                    raise ValueError(f"Could not find boost_system library in BOOST_ROOT ({boost_root})")
             else:
-                raise ValueError(f"Could not find libraries with BOOST_ROOT={boost_root}")
+                for lib in find_boost_system_lib_unix(boost_root):
+                    lib_dir = os.path.dirname(lib)
+                    ringbufcy_ext.extra_link_args.append(f"-L{lib_dir}")
+                    lib_name = os.path.splitext(os.path.basename(lib))[0]
+                    # remove lib prefix
+                    lib_name = lib_name[3:]
+                    ringbufcy_ext.libraries = [lib_name]
+                    break
+                else:
+                    raise ValueError(f"Could not find boost_system library in BOOST_ROOT ({boost_root})")
 
         build_ext.build_extensions(self)
 
